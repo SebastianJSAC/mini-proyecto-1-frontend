37f3d004c298f404dd7a434d3ba4c702566de2be
SebastianJSAC <sebasstiancardona@hotmail.com>
2026-03-07 13:21:38 -0500

    Contador y limitacion en descripcion de la Tarea

--- miniproyecto-frontend/src/components/TaskCard.jsx
+++ miniproyecto-frontend/src/components/TaskCard.jsx
@@ -366,6 +366,7 @@ export default function TaskCard({ descripcionInput, tarea, setTasks, API_URL, n
                     <div className="relative w-full">
 
                         <textarea
+                            maxLength={100}
                             className="w-full px-3 py-2 border rounded-lg text-sm outline-none focus:border-emerald-500 min-h-[80px]"
                             placeholder="Descripción de la tarea..."
                             value={editDescripcion}
