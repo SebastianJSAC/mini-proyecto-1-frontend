a7f598eb5ad724085210a703fd4b2b731a0120b9
SebastianJSAC <sebasstiancardona@hotmail.com>
2026-03-06 19:34:54 -0500

    Cambio de Checkbox por Boton de Completado - Camilo

--- miniproyecto-frontend/src/components/TaskCard.jsx
+++ miniproyecto-frontend/src/components/TaskCard.jsx
@@ -295,7 +295,7 @@ export default function TaskCard({ tarea, tasks, setTasks, API_URL }) {
 
                             <button
                                 onClick={toggleSubtask}
-                                className={`px-3 py-1 rounded-md text-xs font-bold transition-all ${tarea.completada
+                                className={`px-3 py-1 rounded-md text-xs font-bold transition-all ${sub.completada
                                         ? "bg-green-100 text-green-700"
                                         : "bg-slate-100 text-slate-600"
                                     }`}
